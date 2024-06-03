8a8319ed291d9bf1d1179ca32e45bd43c588a98f
AuthoRoman <kit.kiram2017@yandex.ru>
2024-06-04 00:05:18 +0300

    addGh-Pages

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 
 module.exports = defineConfig({
-  publicPath: "/",
+  publicPath: `/storeAnnormy/`,
   transpileDependencies: true,
   pages: {
     index: {
